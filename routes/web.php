fca6d278853366421d021834b4c8a126d63b582d
JotaDuarth <duarthvitor9@gmail.com>
2021-11-04 02:21:41 -0300

    confirmar

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controller\HospedeController;
+use App\Http\Controllers\HospedeController;
 
 /*
 |--------------------------------------------------------------------------
