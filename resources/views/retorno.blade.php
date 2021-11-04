72f3aa739a71e43d1438be761f3136e370662138
JotaDuarth <duarthvitor9@gmail.com>
2021-11-04 02:09:04 -0300

    confirmar

--- resources/views/retorno.blade.php
+++ resources/views/retorno.blade.php
@@ -9,6 +9,18 @@ initial-scale=1.0">
 <title>Novo Welcome</title>
 </head>
 <body>
-<h1>Controller chamou view!!!</h1>
+    <h1>Teste index Hospede</h1>
+    <h2>foreach</h2>
+    @foreach ($hospedes as $hospede)
+        {{$hospede->nome}} - {{$hospede->cpf}}<br>
+
+    @endforeach
+    <h2>forelse</h2>
+    @forelse ($hospedes as $hospede)
+        {{$hospede->nome}} - {{$hospede->cpf}}<br>
+    @empty
+        <p>não existem hospedes</p>
+        @endforelse
+        
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ use App\Http\Controller\HospedeController;
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get('/', function () {
+Route::get('/retorno', function () {
     return view('retorno');
 });
 
